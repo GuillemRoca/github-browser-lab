853f7bcee81253466d70791254322fc5d8bdabb4
Guillem Roca <guillemroc@gmail.com>
2021-02-19 20:31:14 +0100

    Add Owner.kt and Repository.kt domain entities.

--- app/src/main/java/dev/guillem/githubbrowserlab/data/entity/OwnerRemote.kt
+++ app/src/main/java/dev/guillem/githubbrowserlab/data/entity/OwnerRemote.kt
@@ -2,9 +2,9 @@ package dev.guillem.githubbrowserlab.data.entity
 
 import com.google.gson.annotations.SerializedName
 
-data class OwnerRemote (
-    @SerializedName("login") val login : String,
-    @SerializedName("id") val id : Int,
-    @SerializedName("avatar_url") val avatarUrl : String,
-    @SerializedName("html_url") val htmlUrl : String,
+data class OwnerRemote(
+    @SerializedName("login") val login: String,
+    @SerializedName("id") val id: Int,
+    @SerializedName("avatar_url") val avatarUrl: String,
+    @SerializedName("html_url") val htmlUrl: String,
 )
---
+++ app/src/main/java/dev/guillem/githubbrowserlab/domain/entity/Repository.kt
@@ -0,0 +1,10 @@
+package dev.guillem.githubbrowserlab.domain.entity
+
+data class Repository(
+    val id: Int,
+    val name: String,
+    val owner: Owner,
+    val htmlUrl: String,
+    val description: String,
+    val fork: Boolean,
+)
