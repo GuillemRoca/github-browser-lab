a9d9de1bcd66095f5f6a1bd435a9545af6b49be6
Guillem Roca <guillemroc@gmail.com>
2021-02-20 17:33:52 +0100

    Create ApplicationModule.kt with provides of retrofit and api service

---
+++ app/src/main/java/dev/guillem/githubbrowserlab/di/ApplicationModule.kt
@@ -0,0 +1,31 @@
+package dev.guillem.githubbrowserlab.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.guillem.githubbrowserlab.data.ApiService
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .client(OkHttpClient.Builder().build())
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+}
