7f6d37ff4f891ad51dfb92acdf8a6bf472aa80bc
Guillem Roca <guillemroc@gmail.com>
2021-02-20 17:36:38 +0100

    Rename entities

--- app/src/main/java/dev/guillem/githubbrowserlab/data/entity/OwnerEntity.kt
+++ app/src/main/java/dev/guillem/githubbrowserlab/data/entity/OwnerEntity.kt
@@ -2,7 +2,7 @@ package dev.guillem.githubbrowserlab.data.entity
 
 import com.google.gson.annotations.SerializedName
 
-data class OwnerRemote(
+data class OwnerEntity(
     @SerializedName("login") val login: String,
     @SerializedName("id") val id: Int,
     @SerializedName("avatar_url") val avatarUrl: String,
--- app/src/main/java/dev/guillem/githubbrowserlab/data/entity/RepositoryEntity.kt
+++ app/src/main/java/dev/guillem/githubbrowserlab/data/entity/RepositoryEntity.kt
@@ -2,10 +2,10 @@ package dev.guillem.githubbrowserlab.data.entity
 
 import com.google.gson.annotations.SerializedName
 
-data class RepositoryRemote(
+data class RepositoryEntity(
     @SerializedName("id") val id: Int,
     @SerializedName("name") val name: String,
-    @SerializedName("owner") val owner: OwnerRemote,
+    @SerializedName("owner") val owner: OwnerEntity,
     @SerializedName("html_url") val htmlUrl: String,
     @SerializedName("description") val description: String,
     @SerializedName("fork") val fork: Boolean,
