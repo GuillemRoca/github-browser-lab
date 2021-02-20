0116f96032be34b8699a067e5b45fe4041f5014a
Guillem Roca <guillemroc@gmail.com>
2021-02-20 17:38:16 +0100

    Rename RemoteDataSource.kt to ReposDataRepository.kt

--- app/src/main/java/dev/guillem/githubbrowserlab/domain/ReposRepository.kt
+++ app/src/main/java/dev/guillem/githubbrowserlab/domain/ReposRepository.kt
@@ -3,6 +3,6 @@ package dev.guillem.githubbrowserlab.domain
 import dev.guillem.githubbrowserlab.domain.entity.Repository
 import io.reactivex.Single
 
-interface RemoteDataSource {
+interface ReposRepository {
     fun getCompanyRepos(): Single<List<Repository>>
 }
