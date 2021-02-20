7a3200211f80d1373ad57bb51116c52a330bbe30
Guillem Roca <guillemroc@gmail.com>
2021-02-20 17:35:50 +0100

    Create OwnerMapper.kt and RepositoryMapper.kt

---
+++ app/src/main/java/dev/guillem/githubbrowserlab/data/mapper/OwnerMapper.kt
@@ -0,0 +1,23 @@
+package dev.guillem.githubbrowserlab.data.mapper
+
+import dev.guillem.githubbrowserlab.data.entity.OwnerEntity
+import dev.guillem.githubbrowserlab.domain.entity.Owner
+import javax.inject.Inject
+
+class OwnerMapper @Inject constructor() : Mapper<OwnerEntity, Owner> {
+    override fun mapFromEntity(type: OwnerEntity): Owner =
+        Owner(
+            login = type.login,
+            id = type.id,
+            avatarUrl = type.avatarUrl,
+            htmlUrl = type.htmlUrl
+        )
+
+    override fun mapToEntity(type: Owner): OwnerEntity =
+        OwnerEntity(
+            login = type.login,
+            id = type.id,
+            avatarUrl = type.avatarUrl,
+            htmlUrl = type.htmlUrl
+        )
+}
---
+++ app/src/main/java/dev/guillem/githubbrowserlab/data/mapper/RepositoryMapper.kt
@@ -0,0 +1,28 @@
+package dev.guillem.githubbrowserlab.data.mapper
+
+import dev.guillem.githubbrowserlab.data.entity.RepositoryEntity
+import dev.guillem.githubbrowserlab.domain.entity.Repository
+import javax.inject.Inject
+
+class RepositoryMapper @Inject constructor(private val ownerMapper: OwnerMapper) :
+    Mapper<RepositoryEntity, Repository> {
+    override fun mapFromEntity(type: RepositoryEntity): Repository =
+        Repository(
+            id = type.id,
+            name = type.name,
+            owner = ownerMapper.mapFromEntity(type.owner),
+            htmlUrl = type.htmlUrl,
+            description = type.description,
+            fork = type.fork
+        )
+
+    override fun mapToEntity(type: Repository): RepositoryEntity =
+        RepositoryEntity(
+            id = type.id,
+            name = type.name,
+            owner = ownerMapper.mapToEntity(type.owner),
+            htmlUrl = type.htmlUrl,
+            description = type.description,
+            fork = type.fork
+        )
+}
