2e72a80fd0948353521e17752c5558bb9168c332
Guillem Roca <guillemroc@gmail.com>
2021-02-21 17:10:04 +0100

    Setup DI and ViewBinding in Main MVVM

--- app/src/main/java/dev/guillem/githubbrowserlab/presentation/main/MainFragment.kt
+++ app/src/main/java/dev/guillem/githubbrowserlab/presentation/main/MainFragment.kt
@@ -1,30 +1,35 @@
 package dev.guillem.githubbrowserlab.presentation.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.guillem.githubbrowserlab.R
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import dev.guillem.githubbrowserlab.databinding.MainFragmentBinding
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
+    private var sourceBinding: MainFragmentBinding? = null
+    private val binding get() = sourceBinding!!
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        sourceBinding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        sourceBinding = null
+    }
 
     companion object {
         fun newInstance() = MainFragment()
-    }
-
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
--- app/src/main/java/dev/guillem/githubbrowserlab/presentation/main/MainViewModel.kt
+++ app/src/main/java/dev/guillem/githubbrowserlab/presentation/main/MainViewModel.kt
@@ -1,7 +1,13 @@
 package dev.guillem.githubbrowserlab.presentation.main
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.guillem.githubbrowserlab.domain.interactor.GetCompanyRepos
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val getCompanyReposUseCase: GetCompanyRepos
+) : ViewModel() {
     // TODO: Implement the ViewModel
 }
