6caeba2a811b21356255362397e76a8e919e1f08
Guillem Roca <guillemroc@gmail.com>
2021-02-22 13:00:57 +0100

    Cleanup custom tab scheme params

--- app/src/main/java/dev/guillem/githubbrowserlab/presentation/tools/browser/CustomTabsBrowser.kt
+++ app/src/main/java/dev/guillem/githubbrowserlab/presentation/tools/browser/CustomTabsBrowser.kt
@@ -10,14 +10,8 @@ import javax.inject.Inject
 
 class CustomTabsBrowser @Inject constructor() : Browser {
     override fun launch(context: Context, url: String) {
-        val params = CustomTabColorSchemeParams.Builder()
-            .setNavigationBarColor(context.getColorFromAttr(R.attr.colorOnPrimary))
-            .setToolbarColor(context.getColorFromAttr(R.attr.colorPrimary))
-            .setSecondaryToolbarColor(context.getColorFromAttr(R.attr.colorPrimaryVariant))
-            .build()
         val customTabsIntent = CustomTabsIntent.Builder()
             .setShareState(CustomTabsIntent.SHARE_STATE_ON)
-            .setDefaultColorSchemeParams(params)
             .setShowTitle(true)
             .build()
         customTabsIntent.launchUrl(context, Uri.parse(url))
