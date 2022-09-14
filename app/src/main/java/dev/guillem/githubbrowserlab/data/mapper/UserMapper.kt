33e39ab3e15aa19a1be5034ac9604abc4c45f56c
Guillem Roca <guillemroc@gmail.com>
2022-09-14 16:08:09 +0200

    Create UserMapper.kt

---
+++ app/src/main/java/dev/guillem/githubbrowserlab/data/mapper/UserMapper.kt
@@ -0,0 +1,33 @@
+package dev.guillem.githubbrowserlab.data.mapper
+
+import dev.guillem.githubbrowserlab.data.model.OwnerEntity
+import dev.guillem.githubbrowserlab.data.model.UserEntity
+import dev.guillem.githubbrowserlab.domain.entity.Owner
+import dev.guillem.githubbrowserlab.domain.entity.User
+import javax.inject.Inject
+
+class UserMapper @Inject constructor() : Mapper<UserEntity, User> {
+    fun mapFromArray(type: Array<String>) =
+        UserEntity(
+            name = type[0],
+            address = type[1],
+            phone = type[2],
+            email = type[3]
+        )
+
+    override fun mapFromEntity(type: UserEntity): User =
+        User(
+            name = type.name,
+            address = type.address,
+            phone = type.phone,
+            email = type.email
+        )
+
+    override fun mapToEntity(type: User): UserEntity =
+        UserEntity(
+            name = type.name,
+            address = type.address,
+            phone = type.phone,
+            email = type.email
+        )
+}
