64fef0a56be3fe28fa26fcc2d2c7ff6a6b0ecb79
Guillem Roca <guillemroc@gmail.com>
2022-09-14 16:12:00 +0200

    Delete RepositoryClickListener.kt

--- app/src/main/java/dev/guillem/githubbrowserlab/presentation/main/RepositoryClickListener.kt
+++
@@ -1,7 +0,0 @@
-package dev.guillem.githubbrowserlab.presentation.main
-
-import dev.guillem.githubbrowserlab.presentation.model.RepositoryView
-
-interface RepositoryClickListener {
-    fun onRepositoryLongClick(repositoryView: RepositoryView)
-}
