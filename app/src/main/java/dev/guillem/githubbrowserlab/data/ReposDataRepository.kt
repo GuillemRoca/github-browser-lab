d989a4917c92899f70b7e087bf422f8e59e44487
Guillem Roca <guillemroc@gmail.com>
2022-09-14 16:10:18 +0200

    Update ReposDataRepository.kt

--- app/src/main/java/dev/guillem/githubbrowserlab/data/ReposDataRepository.kt
+++ app/src/main/java/dev/guillem/githubbrowserlab/data/ReposDataRepository.kt
@@ -1,14 +1,24 @@
 package dev.guillem.githubbrowserlab.data
 
+import android.content.Context
+import android.util.Log
+import dev.guillem.githubbrowserlab.R
 import dev.guillem.githubbrowserlab.data.mapper.RepositoryMapper
+import dev.guillem.githubbrowserlab.data.mapper.UserMapper
 import dev.guillem.githubbrowserlab.domain.ReposRepository
 import dev.guillem.githubbrowserlab.domain.entity.Repository
+import dev.guillem.githubbrowserlab.domain.entity.User
 import io.reactivex.Single
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 import javax.inject.Inject
 
 class ReposDataRepository @Inject constructor(
     private val apiService: ApiService,
     private val repositoryMapper: RepositoryMapper,
+    private val usersDataSource: UsersDataSource,
+    private val userMapper: UserMapper,
 ) : ReposRepository {
     override fun getCompanyRepos(): Single<List<Repository>> = apiService.getCompanyRepos()
         .map { list ->
@@ -16,4 +26,9 @@ class ReposDataRepository @Inject constructor(
                 repositoryMapper.mapFromEntity(listItem)
             }
         }
+
+    override fun getUsersFromCSV(): Single<List<User>> =
+        Single.just(usersDataSource.getUsersFromCSV().map { user ->
+            userMapper.mapFromEntity(user)
+        })
 }
